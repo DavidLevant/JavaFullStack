424f154cb12c31ffed1e091cd756bf176447b7d8
DavidLevant <104701459+DavidLevant@users.noreply.github.com>
2022-05-16 19:56:40 -0400

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,137 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class LinkedList<T> implements List<T> {
+
+	@Override
+	public void add(T item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(Object item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T remove(Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(T item, int index) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object item) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @return the first item in this linked list
+	 * @throws NoSuchElementException
+	 *             if this linked list is empty
+	 */
+	public T getFirst() {
+		// TODO method stub
+		return null;
+	}
+
+	/**
+	 * @return the last item in this linked list
+	 * @throws NoSuchElementException
+	 *             if this linked list is empty
+	 */
+	public T getLast() {
+		// TODO method stub
+		return null;
+	}
+
+	/**
+	 * Adds 'item' to front of this linked list
+	 * 
+	 * @param item
+	 */
+	public void addFirst(T item) {
+		// TODO method stub
+
+	}
+
+	/**
+	 * Adds 'item' to the end of this linked list
+	 * 
+	 * @param item
+	 */
+	public void addLast(T item) {
+		// TODO method stub
+
+	}
+
+	/**
+	 * Removes and returns the first item from this linked list
+	 * 
+	 * @return
+	 * @throws NoSuchElementException
+	 *             if this linked list is empty
+	 */
+	public T removeFirst() {
+		// TODO method stub
+		return null;
+	}
+
+	/**
+	 * Removes and returns the last item from this linked list
+	 * 
+	 * @return
+	 * @throws NoSuchElementException
+	 *             if this linked list is empty
+	 */
+	public T removeLast() {
+		// TODO method stub
+		return null;
+	}
+	
+	
+}
