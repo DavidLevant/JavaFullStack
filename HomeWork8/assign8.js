dbd75088f8732d6b61db7c05f7ea0d6cd1300e69
DavidLevant <104701459+DavidLevant@users.noreply.github.com>
2022-05-26 19:59:00 -0400

    Add files via upload

---
+++ HomeWork8/assign8.js
@@ -0,0 +1,18 @@
+
+document.getElementById("btnPrimary").onclick = function(){
+
+    
+
+    var w =document.getElementById("pName").value;
+    var x =document.getElementById("character").value;
+    var y =document.getElementById("adverb").value;
+    var z =document.getElementById("adjective").value;
+
+    document.getElementById("output2").innerHTML= "Alright so one day, my friend " + w +  " was visiting New York \
+    and ran into the wall." + x +"." + w + " knew that he was definitly in the wrong part of the store. But " + x + " turned out \
+    to be very " + z + " and " + w + " had 50 dollars.";
+    
+    
+};
+
+
